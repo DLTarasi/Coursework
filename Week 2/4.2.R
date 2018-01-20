b96d391f7924b547ad34e2cd6afbfac1e2220bcf
David Tarasi <dave@Davids-MacBook-Pro.local>
2018-01-19 22:23:01 -0700

    week2

--- Week 2/4.2.R
+++ Week 2/4.2.R
@@ -6,7 +6,13 @@ head(iris)
 summary(iris)
 class(iris)
 #initialize model
-clusters <- kmeans(iris[,3:4], centers=3)
-clusters
+iris_clusters <- kmeans(iris[,3:4], centers=3)
+iris_clusters
+#create table of clusters and species
+Clusters <- as.factor(iris_clusters$cluster)
+table(Clusters, iris$Species)
+#plot petal length vs. width, color points by species
+ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
+#plot petal length vs. width, color points by cluster
+ggplot(iris, aes(Petal.Length, Petal.Width, color = Clusters)) + geom_point()
 
-ggplot2(iris, aes(Petal.Length, Petal.Width, color = Species)) + geom_point()
