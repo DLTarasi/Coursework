4c0fa849525d9d09e2faf931caa6b5ef04eea8c2
David Tarasi <dltarasi@gmail.com>
2018-01-23 20:25:14 -0700

    week2

--- Week 2/3.1.a.R
+++ Week 2/3.1.a.R
@@ -1,4 +1,4 @@
-#
+library(data.table)
 library("kknn")
 #set Directory to data path
 setwd('/Users/dave/isye6501/Week 2')
@@ -8,32 +8,24 @@ data<-read.table("credit_card_data.txt", header = FALSE)
 class(data)
 head(data)
 summary(data)
-#use train.kknn to find best k value. train.kknn uses leave one out
-#cross validation. 
-cv.kknn(formula=V11~.,data=data,kmax=100, kernel="rectangular", distance=2,scale=TRUE)
-#generate train/test/val split
 set.seed(10) # Set seed used in sampling so that we can reproduce sample
-# sample 70% of data and assign to train set. Split remaining 30% 2/3 to validation
-# and 1/3 to test set (so 20% of total to validation, 10% of total data set to test).
+#generate train/test split
+# sample 85% of data and assign to train set. Assign remaining 15% to test set
 sample <- sample.int(n = nrow(data), size = floor(.7*nrow(data)), replace = FALSE)
-sample2 <- sample.int(n = nrow(data[-sample,]), size = floor(.66*nrow(data[-sample,])), replace = FALSE)
 train <- data[sample, ]
-val <- data[sample2,]
-test  <- data[-sample, ][-sample2,]
-#create model - try a variety of hyperparameters using the validation set
-model<-kknn(V11~.,train,val,k=2,distance=2,kernel="rectangular")
-#get predictions and convert from continuous reponse to binary by rounding to 0 or 1
-predictions<-round(fitted(model))
-predictions
-# generate confusion matrix - 0,0 means model predicts 0 and actual was 0
-confusion <- table(val[,11], predictions)
-confusion
-# Compute accuracy on val
-accu <- sum(diag(confusion))/nrow(val)
-accu
+test  <- data[-sample, ]
+#use cv.kknn to to perform k-fold cross validation. Test variety of Ks.
+cv<-cv.kknn(formula=V11~.,data=train, k=13, kernel="rectangular", distance=2,scale=TRUE, kcv = 10)
+cv = data.table(cv[[1]])
+cv$yhat <- round(cv$yhat)
+cv
+# Cross Validation Accuracy - compare actual vs. predicted
+table(cv$y == cv$yhat)
+prop.table(table(cv$y == cv$yhat))
+
 
 #final test with best hyperparameters
-model<-kknn(V11~.,train,test,k=2,distance=2,kernel="rectangular")
+model<-kknn(V11~.,train,test,k=13,distance=2,kernel="rectangular")
 #get predictions and convert from continuous reponse to binary by rounding to 0 or 1
 predictions<-round(fitted(model))
 predictions
