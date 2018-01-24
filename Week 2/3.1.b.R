08f7ce45c9bd7f24095183ca160d609a4117a755
David Tarasi <dltarasi@gmail.com>
2018-01-23 20:44:10 -0700

    week2

--- Week 2/3.1.b.R
+++ Week 2/3.1.b.R
@@ -1,4 +1,3 @@
-#
 library("kknn")
 #set Directory to data path
 setwd('/Users/dave/isye6501/Week 2')
@@ -8,20 +7,17 @@ data<-read.table("credit_card_data.txt", header = FALSE)
 class(data)
 head(data)
 summary(data)
-#use train.kknn to find best k value. train.kknn uses leave one out
-#cross validation. 
-cv.kknn(formula=V11~.,data=data,kmax=100, kernel="rectangular", distance=2,scale=TRUE)
 #generate train/test/val split
 set.seed(10) # Set seed used in sampling so that we can reproduce sample
-# sample 70% of data and assign to train set. Split remaining 30% 2/3 to validation
-# and 1/3 to test set (so 20% of total to validation, 10% of total data set to test).
+# sample 70% of data and assign to train set. Split remaining 30% 1/2 to validation
+# and 1/2 to test set (so 15% of total to validation, 15% of total data set to test).
 sample <- sample.int(n = nrow(data), size = floor(.7*nrow(data)), replace = FALSE)
-sample2 <- sample.int(n = nrow(data[-sample,]), size = floor(.66*nrow(data[-sample,])), replace = FALSE)
+sample2 <- sample.int(n = nrow(data[-sample,]), size = floor(.5*nrow(data[-sample,])), replace = FALSE)
 train <- data[sample, ]
 val <- data[sample2,]
 test  <- data[-sample, ][-sample2,]
 #create model - try a variety of hyperparameters using the validation set
-model<-kknn(V11~.,train,val,k=2,distance=2,kernel="rectangular")
+model<-kknn(V11~.,train,val,k=13,distance=2,kernel="rectangular")
 #get predictions and convert from continuous reponse to binary by rounding to 0 or 1
 predictions<-round(fitted(model))
 predictions
@@ -33,7 +29,7 @@ accu <- sum(diag(confusion))/nrow(val)
 accu
 
 #final test with best hyperparameters
-model<-kknn(V11~.,train,test,k=2,distance=2,kernel="rectangular")
+model<-kknn(V11~.,train,test,k=13,distance=2,kernel="rectangular")
 #get predictions and convert from continuous reponse to binary by rounding to 0 or 1
 predictions<-round(fitted(model))
 predictions
