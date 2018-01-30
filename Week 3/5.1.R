82821a6d9e68a09301eded625abc74b02638badd
David Tarasi <dltarasi@gmail.com>
2018-01-30 10:27:32 -0700

    week 3

--- Week 3/5.1.R
+++ Week 3/5.1.R
@@ -15,5 +15,11 @@ g + geom_boxplot(varwidth=T, fill="plum") +
        x="City",
        y="Crime")
 #single sided grubs tests
-?grubbs.test
 grubbs.test(data$Crime, type = 10)
+# 1993 is an outlier at a p-value of .1, but not at .05
+data<-data[-26,]
+grubbs.test(data$Crime, type = 10)
+# 1969 is also an outlier at p-value of .1 once 1993 is removed
+data<-data[-4,]
+grubbs.test(data$Crime, type = 10)
+#1674 is not an outlier at p-value of of .1
