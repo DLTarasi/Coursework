32462490d3803fb4af0ae5ea8f2d86bcfbe4b295
David Tarasi <dltarasi@gmail.com>
2018-02-05 21:33:47 -0700

    week 4

--- Week 4/7.2.R
+++ Week 4/7.2.R
@@ -8,16 +8,24 @@ class(data)
 head(data)
 summary(data)
 plot.ts(data[,2:5])
-#convert each year in data to ts object
+#convert each year in data to timeseries object
 data<-as.vector(unlist(data[,2:21]))
 ts <- ts(data, start = 1996, frequency=123)
 ts
-hw<-HoltWinters(ts, seasonal = c("multiplicative"),
-                optim.start = c(alpha = .5, beta = .5, gamma = .5))
+#run HoltWinters on the timeseries, using Alpha, Beta, and Gamma, multiplicative seasonality to try to capture non-linearity in data
+hw<-HoltWinters(ts, seasonal = c("multiplicative"))
 hw
+#Plot the smoothed data
 plot(hw)
+#see what the cooeficient for gamma is
 hw$gamma
+#plot the fitted factors for the model
+plot(hw$fitted)
+#store the value of the seasonality adjustment in a single vector.
 seasonality<-hw$fitted[,4]
+#plot seasonality adjustment -increases in later years.
 plot(seasonality)
-cusum(seasonality, decision.interval = 150, se.shift = 1)
+# export all data 
 write.table(seasonality, "/Users/dave/isye6501/Week 4/seasonality.txt", sep="\t")
+seasonality_by_year <- matrix(hw$fitted[,4],ncol=19)
+write.table(seasonality_by_year, "/Users/dave/isye6501/Week 4/seasonality_by_year.txt", sep="\t")
