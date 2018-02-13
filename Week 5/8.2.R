2ae7fa4929037ce3ed557e2ce3e2ba9fcfa77034
David Tarasi <dltarasi@gmail.com>
2018-02-13 15:56:45 -0700

    week 5

--- Week 5/8.2.R
+++ Week 5/8.2.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 library(outliers)
+library(reshape2)
 #set Directory to data path
-setwd('/Users/dave/isye6501/Week 3')
+setwd('/Users/dave/isye6501/Week 5')
 #create table of crime data with headers
 uscrimedata<-read.table("uscrime.txt", header = TRUE)
 #Check class and inspect data
@@ -18,21 +19,29 @@ grubbs.test(uscrimedata$Crime, type = 10)
 #data<-data[-4,]
 #grubbs.test(uscrimedata$Crime, type = 10)
 #1674 is not an outlier at p-value of of .1
-ggplot(data) + geom_point(aes(x=U2, y=Crime))
-model <- lm(Crime ~., data)
-summary(model)
-model <- lm(Crime ~ Ed + U2 + Ineq + Prob, data)
-summary(model)
 
-test_city <- c()
-
-new_crime = predict(object=model, newdata = test_city)
-
-uscrime_melt = melt(data=uscrime, measure.vars = colnames(uscrime[,1:15]))
-
-citycrime_melt = melt(data=citycrime, measure.vars = colnames(citycrime[,1:15]))
-
+#plot all variables
+uscrime_melt = melt(data=uscrimedata, measure.vars = colnames(uscrimedata[,1:15]))
 ggplot(data = uscrime_melt, aes(x=value, y=Crime)) +
   geom_point() +
-  geom_point(data=citycrime_melt, color = "red") +
   facet_wrap(~variable, scales = "free")
+#Create model with all variables
+crimemodel <- lm(Crime ~., uscrimedata)
+summary(crimemodel)
+#create model with only variables with p value below .1
+crimemodel <- lm(Crime ~ M + Ed + Po1 + U2 + Ineq + Prob, uscrimedata)
+summary(crimemodel)
+#create test city data frame
+test_city <- c(M = 14.0, So = 0,
+               Ed = 10.0, Po1 = 12.0, 
+               Po2 = 15.5,LF = 0.640,
+               M.F = 94.0, Pop = 150,
+               NW = 1.1, U1 = 0.120,
+               U2 = 3.6, Wealth = 3200,
+               Ineq = 20.1, Prob = 0.04, 
+               Time = 39.0)
+test_city <- t(as.data.frame(test_city))
+
+new_crime = predict(object=crimemodel, newdata = test_city)
+
+
