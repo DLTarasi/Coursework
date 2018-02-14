107eed94bf69f00e92625bc4706f06d8699d8316
David Tarasi <dltarasi@gmail.com>
2018-02-14 09:50:29 -0700

    week 5

--- Week 5/8.2.R
+++ Week 5/8.2.R
@@ -1,3 +1,4 @@
+#packages
 library(ggplot2)
 library(outliers)
 library(reshape2)
@@ -25,14 +26,58 @@ uscrime_melt = melt(data=uscrimedata, measure.vars = colnames(uscrimedata[,1:15]
 ggplot(data = uscrime_melt, aes(x=value, y=Crime)) +
   geom_point() +
   facet_wrap(~variable, scales = "free")
+
+
+########Build Model
 #Create model with all variables
-crimemodel <- lm(Crime ~., uscrimedata)
-summary(crimemodel)
-AIC(crimemodel)
-#create model with only variables with p value below .1
-crimemodel <- lm(Crime ~ M + Ed + Po1 + U2 + Ineq + Prob, uscrimedata)
-summary(crimemodel)
-AIC(crimemodel)
+basecrimemodel <- lm(Crime ~., uscrimedata)
+summary(basecrimemodel)
+AIC(basecrimemodel)
+BIC(basecrimemodel)
+
+#create model with only variables with p value below .1 - chose this one
+crimemodelp.1 <- lm(Crime ~ M + Ed + Po1 + U2 + Ineq + Prob, uscrimedata)
+summary(crimemodelp.1)
+AIC(crimemodelp.1)
+BIC(crimemodelp.1)
+
+#create model with only variables with p value below .05
+crimemodelp.05 <- lm(Crime ~ M + Ed + Ineq + Prob, uscrimedata)
+summary(crimemodelp.05)
+AIC(crimemodelp.05)
+BIC(crimemodelp.05)
+
+
+##########check for multicolinearity
+#create correlation matrix
+cormat <- round(cor(uscrimedata),2)
+# Get upper triangle of the correlation matrix
+get_upper_tri <- function(cormat){
+  cormat[lower.tri(cormat)]<- NA
+  return(cormat)
+}
+upper_tri <- get_upper_tri(cormat)
+melted_cormat <- melt(upper_tri, na.rm = TRUE)
+#plot correlation matrix
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()+
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4)
+
+######Remove cross correlated variables from p .1 model
+#Try the above model without education (higher p value of two correlated variables ed and ineq) - results are worse, use original.
+crimemodelp.1v2 <- lm(Crime ~ M + Po1 + U2 + Ineq + Prob, uscrimedata)
+summary(crimemodelp.1v2)
+AIC(crimemodelp.1v2)
+BIC(crimemodelp.1v2)
+
+########Test
 #create test city data frame
 test_city <- c(M = 14.0, So = 0,
                Ed = 10.0, Po1 = 12.0, 
@@ -43,7 +88,9 @@ test_city <- c(M = 14.0, So = 0,
                Ineq = 20.1, Prob = 0.04, 
                Time = 39.0)
 test_city <- as.data.frame(t(test_city))
-#predict crime level in test city using the crime model
-new_crime = predict(object=crimemodel, newdata = test_city)
+
+#predict crime level in test city using the crime model of variables with p value below .1- Result 1304.245 - higher than all but 5 of the samples
+new_crime = predict(object=crimemodelp.1, newdata = test_city)
+
 
 
