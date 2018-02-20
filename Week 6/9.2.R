3f6f038861b67f0790770421eb94503db2b7f9fb
David Tarasi <dltarasi@gmail.com>
2018-02-20 14:53:42 -0700

    week 6

--- Week 6/9.2.R
+++ Week 6/9.2.R
@@ -1,1 +1,49 @@
+#packages
+library(pls)
+library(ggplot2)
+library(reshape2)
+#set Directory to data path
+setwd('/Users/dave/isye6501/Week 6')
+#create table of crime data with headers
+uscrimedata<-read.table("uscrime.txt", header = TRUE)
+#Check class and inspect data
+class(uscrimedata)
+head(uscrimedata)
+summary(uscrimedata)
+
+
+
+#####Plotting and Data Cleaning
+#####Outlier Testing - did not remove potential outliers based on week three analysis
+#plot all variables
+uscrime_melt = melt(data=uscrimedata, measure.vars = colnames(uscrimedata[,1:15]))
+ggplot(data = uscrime_melt, aes(x=value, y=Crime)) +
+  geom_point() +
+  facet_wrap(~variable, scales = "free")
+#Remove So because it is categorical - PCA works best on continuos numerical data
+uscrimedata <- uscrimedata[,-2]
+
+########Build Model
+#Find principal components of dataset
+pcamodel <- pcr(Crime ~., data = uscrimedata, scale = TRUE)
+summary(pcamodel)
+pcacoef<-as.data.frame(coef(pcamodel))
+
+########Predict
+#create test city data frame - do not use So as it was not used when creating principal components
+test_city <- c(M = 14.0,
+               Ed = 10.0, Po1 = 12.0, 
+               Po2 = 15.5,LF = 0.640,
+               M.F = 94.0, Pop = 150,
+               NW = 1.1, U1 = 0.120,
+               U2 = 3.6, Wealth = 3200,
+               Ineq = 20.1, Prob = 0.04, 
+               Time = 39.0)
+test_city <- as.data.frame(t(test_city))
+
+#predict crime level in test city using the crime model with X principal components
+new_crime = predict(object=pcacoef, newdata = test_city)
+
+
+####### convert back to original vars
 pca_output$rotation %*% lm_model_from_pca$coefficients[-1]
